4bd5b78925d8a53b1bb69fc48796f4b250b5efea
[okoro progress] <[progressewokoro@gmail.com]>
2023-04-24 17:58:50 +0100

    i did changes and an ugly interactive stuff that's invinsible

--- boss.js
+++ boss.js
@@ -1,1 +1,15 @@
 alert("i am mr another")
+
+let sub = document.getElementById("sub");
+
+//  function vital(){
+//     sub.textContent = "what did you think clicking on \" SUB\" will do for you";
+// }
+
+
+sub.addEventListener("click",  function (){
+    sub.textContent = "what did you think clicking on \" SUB\" will do for you";
+    sub.style.width = "auto";
+    sub.style.backgroundColor = "pink"
+    sub.style.color = "#000"
+});
